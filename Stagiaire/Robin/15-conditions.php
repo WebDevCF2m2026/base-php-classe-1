b820c0f62c9cbba4626a73f58fccb9224428bf45
KukichaBXL <craeyerobin@gmail.com>
2026-03-05 10:28:37 +0100

    EX15b

--- Stagiaire/Robin/15-conditions.php
+++ Stagiaire/Robin/15-conditions.php
@@ -5,6 +5,9 @@ if ($note >= 0 && $note <= 3) {
     echo "{$note} : Nul, étudie la prochaine fois";
 } elseif ($note >= 4 && $note <= 5) {
     echo "{$note} : Peut mieux faire";
-} elseif ($note >= 8 && $note <= 10) {
+  } elseif ($note >= 6 && $note <= 7) {
+    echo "{$note} : Bien";
+  }
+elseif ($note >= 8 && $note <= 10) {
     echo "{$note} : Très bien";
 }
