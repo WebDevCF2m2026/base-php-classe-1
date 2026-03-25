10d8d24722a811550db72e185732c530da119007
julia-dm <yulia.2508dmitruk@gmail.com>
2026-03-25 12:25:31 +0100

    new file:   Stagiaire/Michael/mvc-19/datas/a-propos.html         new file:   Stagiaire/Michael/mvc-19/datas/competences.html         new file:   Stagiaire/Michael/mvc-19/datas/contact.html         new file:   Stagiaire/Michael/mvc-19/datas/css/style.css         new file:   Stagiaire/Michael/mvc-19/datas/images/projet1.jpg         new file:   Stagiaire/Michael/mvc-19/datas/images/projet2.jpg         new file:   Stagiaire/Michael/mvc-19/datas/images/projet3.jpg         new file:   Stagiaire/Michael/mvc-19/datas/index.html         new file:   Stagiaire/Michael/mvc-19/datas/js/script.js         new file:   Stagiaire/Michael/mvc-19/datas/projets.html         modified:   Stagiaire/Yuliia/mvc-19/config.php         new file:   Stagiaire/Yuliia/mvc-19/data/a-propos.html         new file:   Stagiaire/Yuliia/mvc-19/data/competences.html         new file:   Stagiaire/Yuliia/mvc-19/data/contact.html         new file:   Stagiaire/Yuliia/mvc-19/data/css/style.css         new file:   Stagiaire/Yuliia/mvc-19/data/images/projet1.jpg         new file:   Stagiaire/Yuliia/mvc-19/data/images/projet2.jpg         new file:   Stagiaire/Yuliia/mvc-19/data/images/projet3.jpg         new file:   Stagiaire/Yuliia/mvc-19/data/index.html         new file:   Stagiaire/Yuliia/mvc-19/data/js/script.js         new file:   Stagiaire/Yuliia/mvc-19/data/projets.html         new file:   Stagiaire/Yuliia/mvc-19/public/css/style.css         new file:   Stagiaire/Yuliia/mvc-19/public/images/projet1.jpg         new file:   Stagiaire/Yuliia/mvc-19/public/images/projet2.jpg         new file:   Stagiaire/Yuliia/mvc-19/public/images/projet3.jpg         modified:   Stagiaire/Yuliia/mvc-19/public/index.php         new file:   Stagiaire/Yuliia/mvc-19/public/js/script.js         new file:   Stagiaire/Yuliia/mvc-19/view/404.php         new file:   Stagiaire/Yuliia/mvc-19/view/a-propos.php         new file:   Stagiaire/Yuliia/mvc-19/view/competences.php         new file:   Stagiaire/Yuliia/mvc-19/view/contact.php         new file:   Stagiaire/Yuliia/mvc-19/view/inc/footer.php         new file:   Stagiaire/Yuliia/mvc-19/view/inc/navbar.php         new file:   Stagiaire/Yuliia/mvc-19/view/index.php         new file:   Stagiaire/Yuliia/mvc-19/view/projets.php

--- Stagiaire/Yuliia/mvc-19/config.php
+++ Stagiaire/Yuliia/mvc-19/config.php
@@ -7,6 +7,14 @@
 # __DIR__ constante magique qui donnera
 # le chemin absolu vers la racine du projet 
 const ROOT_PATH=__DIR__;
+
+// constante contenant les pages accessibles sans être connecté
+// dans une application réelle, on y mettra les pages accetées par les utilisateurs non authentifiés via une variable get 
 const PUBLIC_PAGES = [
-    
+   //inutile car identique que sans
+   // variables GET 'index',
+   'a-propos',
+   'competences',
+   'projets',
+   'contact'
 ];
