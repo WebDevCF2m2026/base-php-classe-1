b8e08543efdf848989112b7126f3ceeb25d410a0
TNK-Sempaï <alioussalah.meidhy@gmail.com>
2026-04-17 10:01:04 +0200

    Ex20 et 21

---
+++ Stagiaire/Meidhy/Exercice/21-calculette.php
@@ -0,0 +1,26 @@
+<?php
+function calculSimple($a,$operateur,$b) {
+    if ($operateur === "+") {
+        return $res = ($a + $b);
+    } elseif ($operateur === "-") {
+        return $res = ($a - $b);
+    } elseif ($operateur === "*") {
+        return $res = ($a * $b);
+    } elseif ($operateur === "/") { 
+        if ($a == 0 || $b == 0)
+            return $res = "Vous ne pouvez pas divisez par 0";
+        return $res = ($a / $b); 
+    } else {
+        return $res = "Il y a un probleme";
+    }
+}
+
+
+$res = "";
+$premiereValeur = rand(1,10000);
+$deuxiemeValeur = rand(1,10000);
+$opera = "+";
+// if (calculSimple(2,"+",6)) {
+    $res .= calculSimple($premiereValeur,$opera,$deuxiemeValeur);
+    echo "{$premiereValeur} {$opera} {$deuxiemeValeur} = {$res}";
+//}
