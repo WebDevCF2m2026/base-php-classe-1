090cc0c0fe97f662a998bd4b1f697ffbd0565172
Tao <crypto.tao@hotmail.com>
2026-04-21 12:01:06 +0200

    exercice 8

---
+++ Stagiaire/Tawfik/08-types.php
@@ -0,0 +1,16 @@
+<?php
+$chaine = "3,14";
+
+//convertir en entier
+$entier = (int) $chaine;
+echo $chaine;
+//convertir en decimal
+$decimal = (float) $chaine;
+//echo $chaine;
+//convertir en boolean
+$boolean = (bool) $chaine;
+//echo $chaine;
+//convertir en tableau
+$tableau = (array) $chaine;
+//convertir en redevientChaine
+$redevientChaine = (string) 42;
